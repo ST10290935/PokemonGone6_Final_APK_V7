641b135f9c0f746b066f47a5a7818edb410bc59a
ST10248202 <128582074+ST10248202@users.noreply.github.com>
2025-10-07 18:01:29 +0200

    Added code attribution to LoginActivity.kt

--- app/src/main/java/com/example/pokemon/LoginActivity.kt
+++ app/src/main/java/com/example/pokemon/LoginActivity.kt
@@ -1,3 +1,12 @@
+//Code Attribution:
+//For the code below these are the sources I have used to improve my knowledge and implement features:
+//Android Developers, 2025. Authenticate users with Sign in with Google. [online] Available at: <https://developer.android.com/identity/sign-in/credential-manager-siwg> [Accessed 29 September 2025].
+//Firebase, 2025. Get Started with Firebase Authentication on Android. [online] Available at: <https://firebase.google.com/docs/auth/android/start> [Accessed 29 September 2025].
+//CodingSTUFF, 2022. Google Sign In using Firebase in Kotlin (Android Studio 2022). [video online] Available at: <https://youtu.be/318sOlkJBQ?si=jrFTWSEeZvbKk-nS> [Accessed 25 September 2025].
+//Codes Easy, 2022. Login and Registration using Firebase in Android. [video online] Available at: <https://youtu.be/QAKq8UBv4GI?si=8ZBHqVJt5GyqiwNY> [Accessed 25 September 2025].
+//Coding World, 2025. How To Generate SHA1 Key In Android Studio [2024] | 100% Working Method. [video online] Available at: <https://youtu.be/ClLZTrvsUSk?si=jiXRUUooIEsmJMiz> [Accessed 27 September 2025].
+
+
 package com.example.pokemon
 
 import android.content.Intent
