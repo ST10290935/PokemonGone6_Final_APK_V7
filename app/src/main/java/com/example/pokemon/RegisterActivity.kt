a04054f8a0031d3313979b5d6ebe10c0da09e818
ST10248202 <128582074+ST10248202@users.noreply.github.com>
2025-10-07 18:06:31 +0200

    Added code attribution to RegisterActivity.kt

--- app/src/main/java/com/example/pokemon/RegisterActivity.kt
+++ app/src/main/java/com/example/pokemon/RegisterActivity.kt
@@ -1,3 +1,9 @@
+//Code Attribution:
+//For the code below these are the sources I have used to improve my knowledge and implement features:
+//Firebase, 2025. Get Started with Firebase Authentication on Android. [online] Available at: <https://firebase.google.com/docs/auth/android/start> [Accessed 29 September 2025].
+//Codes Easy, 2022. Login and Registration using Firebase in Android. [video online] Available at: <https://youtu.be/QAKq8UBv4GI?si=8ZBHqVJt5GyqiwNY> [Accessed 25 September 2025].
+
+
 package com.example.pokemon
 
 import android.content.Intent
