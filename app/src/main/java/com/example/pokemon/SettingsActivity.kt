bba751161e8002f98c7d950acc8bfc1eb36df5db
ST10248202 <128582074+ST10248202@users.noreply.github.com>
2025-10-07 18:12:37 +0200

    Added code attribution to SettingsActivity.kt

--- app/src/main/java/com/example/pokemon/SettingsActivity.kt
+++ app/src/main/java/com/example/pokemon/SettingsActivity.kt
@@ -1,3 +1,8 @@
+//Code Attribution:
+//For the code below these are the sources I have used to improve my knowledge and implement features:
+//GeeksForGeeks, 2025. How to implement preferences settings screen in Android?. [online] Available at: <https://www.geeksforgeeks.org/android/how-to-implement-preferences-settings-screen-in-android/> [Accessed 29 September 2025].
+
+
 package com.example.pokemon
 
 import android.os.Bundle
