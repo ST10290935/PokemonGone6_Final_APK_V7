c3bccd7ab0e900c3ed92cc4d3dc7b034e3a2ceca
ST10248202 <128582074+ST10248202@users.noreply.github.com>
2025-11-17 14:09:24 +0200

    Added code attribution comments for BaseActivity.kt
    Added code attribution and sources for localization guidance.

--- app/src/main/java/com/example/pokemon/BaseActivity.kt
+++ app/src/main/java/com/example/pokemon/BaseActivity.kt
@@ -1,3 +1,10 @@
+//Code Attribution
+// For the below code I have used these sources to guide me:
+//Codex Creator, 2023. How To Translate Apps In Android Studio. [video online] Available at: < https://youtu.be/v_ohTJnyNAA?si=-FVjgedLgWnnOQEq> [Accessed 16 November 2025].
+//Mobile App Development, 2024. How to Translate Your App to Different Languages | Localization in Android | Android Studio. [video online] Available at: < https://youtu.be/qqBPnKCwLn8?si=iGzG6D9LtR6_x_E_> [Accessed 16 November 2025].
+//Phillip Lackner, 2021. How to Translate Your Android App to Any Language (SO EASY!) - Android Studio Tutorial. [video online] Available at: < https://youtu.be/LXbpsBtIIeM?si=O4LH5rggEhoaGKXO> [Accessed 15 November 2025].
+
+
 package com.example.pokemon
 
 import android.content.Context
